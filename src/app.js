fa5ddc231f66197bbea069f60da95c1ccbb6ef85
SegoviaLourdes <segovia.lourdes1309@gmail.com>
2023-08-09 00:38:11 -0300

    corrigiendoapp.js

--- src/app.js
+++ src/app.js
@@ -7,7 +7,6 @@ app.use(express.static("public"));
 const port =process.env.PORT || 3001;
 app.listen(port,()=>console.log(`servidor corriendo en el puerto ${port}`));
 
-app.listen(3001,()=>"sevidor escuchando en puerto 3001")
 
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname, "./views/home.html"))
